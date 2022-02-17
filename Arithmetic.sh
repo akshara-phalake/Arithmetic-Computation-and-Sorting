3f9064dbfabca74ad024edc9a92b3025348557fe
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-17 13:09:08 +0530

    Added: Computation of a%b+c

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -6,7 +6,9 @@ read -p "Enter the third number : " c
 compute1=$(($a+$b*$c))
 compute2=$(($a*$b+$c))
 compute3=$(($c+$a/$b))
+compute4=$(($a%$b+$c))
 echo $compute1
 echo $compute2
 echo $compute3
+echo $compute4
 
