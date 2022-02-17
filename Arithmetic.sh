d7f1d9190bd741a999f12fd945df92a8ebe33653
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-17 13:03:12 +0530

    Added: Computation of c+a/b

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -5,6 +5,8 @@ read -p "Enter the second number : " b
 read -p "Enter the third number : " c
 compute1=$(($a+$b*$c))
 compute2=$(($a*$b+$c))
+compute3=$(($c+$a/$b))
 echo $compute1
 echo $compute2
+echo $compute3
 
