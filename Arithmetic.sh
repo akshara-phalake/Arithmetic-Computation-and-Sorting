ca22cd7b84e314c853186e82baad51aaaaa18e00
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-17 14:34:52 +0530

    Store the results in a dictionary

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -3,6 +3,7 @@
 read -p "Enter the first number : " a
 read -p "Enter the second number : " b
 read -p "Enter the third number : " c
+declare -A results
 compute1=$(($a+$b*$c))
 compute2=$(($a*$b+$c))
 compute3=$(($c+$a/$b))
@@ -11,4 +12,8 @@ echo $compute1
 echo $compute2
 echo $compute3
 echo $compute4
-
+results[compute1]=$compute1
+results[compute2]=$compute2
+results[compute3]=$compute3
+results[compute4]=$compute4
+echo "Results = " ${results[@]}
