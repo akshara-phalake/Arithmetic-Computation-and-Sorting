71ca5aaf3659224c91f5c087aba1150ce346b14c
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-19 19:52:26 +0530

    Store in Dictionary

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -8,12 +8,8 @@ compute1=$(($a+$b*$c))
 compute2=$(($a*$b+$c))
 compute3=$(($c+$a/$b))
 compute4=$(($a%$b+$c))
-echo $compute1
-echo $compute2
-echo $compute3
-echo $compute4
-results[compute1]=$compute1
-results[compute2]=$compute2
-results[compute3]=$compute3
-results[compute4]=$compute4
-echo "Results = " ${results[@]}
+results[1]=$compute1
+results[2]=$compute2
+results[3]=$compute3
+results[4]=$compute4
+echo "Dictionary = " ${results[@]}
