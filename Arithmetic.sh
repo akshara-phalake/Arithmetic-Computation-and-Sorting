6b117625c9937590b8c554272aed649ea2159c36
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-19 19:55:11 +0530

    Dictionary to Array

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -9,16 +9,14 @@ compute1=$(($a+$b*$c))
 compute2=$(($a*$b+$c))
 compute3=$(($c+$a/$b))
 compute4=$(($a%$b+$c))
-results[compute1]=$compute1
-results[compute2]=$compute2
-results[compute3]=$compute3
-results[compute4]=$compute4
+results[1]=$compute1
+results[2]=$compute2
+results[3]=$compute3
+results[4]=$compute4
+echo "Dictionary = " ${results[@]}
 
-function storeIntoArray(){
-for(( index=0; index<${#results[@]}; index++ ))
-	do
-		resultInArray[index]=${results[$index]}
-	done
-}
-
-storeIntoArray
+for((k=1; k<5; k++ ))
+do
+	resultInArray[k]=$(( ${results[$k]} ))
+done
+echo "Array = " ${resultInArray[@]}
